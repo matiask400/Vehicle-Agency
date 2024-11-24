66aca11d3ad6e96716b59b8cacfa2588d17aa00f
luciano bobroviec <95909@sistemas.frc.utn.edu.ar>
2024-11-24 15:58:27 -0300

    Seguridad

---
+++ TpVehiculos-main/TpVehiculosParent/tpVehiculos/src/main/java/com/example/tpVehiculos/config/ResourceServerConfig.java
@@ -0,0 +1,41 @@
+package ar.edu.utn.frc.pruebaAgencia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class ResourceServerConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/agencia/prueba/**").hasRole("EMPLEADO")
+                .requestMatchers("/api/notificaciones/**").hasRole("EMPLEADO")
+                .requestMatchers("/api/agencia/interesado/**").hasRole("VEHICULO")
+                .requestMatchers("/api/agencia/interesado/**").hasRole("USUARIO")
+                .requestMatchers("/api/agencia/reportes/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+}
